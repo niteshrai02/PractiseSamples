79cd2d448f66ef90cd50971a933ae30c5e202d77
nitesh <niteshrai02@gmail.com>
2017-12-25 13:54:33 -0800

    Custom Directive

---
+++ AngularJs_Sample_Practise/Scripts/Angular/Directive/CustomDirective.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../Lib/angular.min.js" />
+app.directive('hello', function () {
+    var directive = {};
+    directive.restrict = 'E';
+    directive.template = 'My First Directive';
+    directive.transclude = true;
+
+    return directive;
+});
