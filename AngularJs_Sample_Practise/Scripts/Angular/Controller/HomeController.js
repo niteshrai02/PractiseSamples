81bc15f2b7eca947488c0f434559bd81a2a5516b
nitesh <niteshrai02@gmail.com>
2017-12-25 00:31:51 -0800

    filter added

--- AngularJs_Sample_Practise/Scripts/Angular/Controller/HomeController.js
+++ AngularJs_Sample_Practise/Scripts/Angular/Controller/HomeController.js
@@ -1,15 +1,18 @@
 ﻿/// <reference path="../Service/HomeService.js" />
 /// <reference path="../Lib/angular.min.js" />
 
-app.controller("HomeController", function ($scope, HomeService, HomeFactory, globalSetting) {
+app.controller("HomeController", function ($scope, HomeService, HomeFactory, globalSetting, $filter) {
     $scope.input1 = 0;
     $scope.input2 = 0;
+    $scope.resultValue = "";
 
     $scope.GetSum = function (val1, val2) {
         $scope.result = HomeService.Sum(val1, val2);
+        $scope.resultValue = $filter('oddeven')($scope.result);
     }
     $scope.Multiply = function (val1, val2) {
         $scope.result = HomeFactory.Multiply(val1, val2);
+        $scope.resultValue = $filter('oddeven')($scope.result);
     }
     $scope.AppName = globalSetting.appname;
 
---
+++ AngularJs_Sample_Practise/Scripts/Angular/Filter/Filter.js
@@ -0,0 +1,9 @@
+﻿app.filter('oddeven', function () {
+    return function (input) {
+        var result = "odd";
+        if (input % 2 == 0) {
+            result = "even";
+        }
+        return result;
+    };
+});
