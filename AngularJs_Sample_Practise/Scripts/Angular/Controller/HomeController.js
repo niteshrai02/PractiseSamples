3c4a24aa2117bd84043dea354b1a4443cf0d4a65
nitesh <niteshrai02@gmail.com>
2017-12-24 23:16:07 -0800

    Service Changes done

--- AngularJs_Sample_Practise/Scripts/Angular/Controller/HomeController.js
+++ AngularJs_Sample_Practise/Scripts/Angular/Controller/HomeController.js
@@ -1,13 +1,16 @@
 ﻿/// <reference path="../Service/HomeService.js" />
 /// <reference path="../Lib/angular.min.js" />
 
-app.controller("HomeController", function ($scope,HomeService) {
+app.controller("HomeController", function ($scope, HomeService, HomeFactory, globalSetting) {
     $scope.input1 = 0;
     $scope.input2 = 0;
 
     $scope.GetSum = function (val1, val2) {
         $scope.result = HomeService.Sum(val1, val2);
     }
-    
+    $scope.Multiply = function (val1, val2) {
+        $scope.result = HomeFactory.Multiply(val1, val2);
+    }
+    $scope.AppName = globalSetting.appname;
 
 });
--- AngularJs_Sample_Practise/Scripts/Angular/Service/HomeService.js
+++ AngularJs_Sample_Practise/Scripts/Angular/Service/HomeService.js
@@ -1,10 +1,36 @@
 ﻿
 /// <reference path="../Module/Module.js" />
+// This is the service that has been created using factory method
 app.factory("HomeFactory", function () {
     var factory = {};
 
-    factory.Sum = function (input1, input2) {
-        return input1 + input2;
+    factory.Multiply = function (input1, input2) {
+        return input1 * input2;
     };
     return factory;
+});
+
+//This is the service that has been created using service method
+app.service("HomeService", function () {
+    //var factory = {};
+
+    this.Sum = function (input1, input2) {
+        var sum = input1 + input2;
+        return sum;
+    };
+    //return factory;
+});
+
+//This is the service that has been created using provider method
+app.config(function ($provide) {
+    $provide.provider('globalSetting', function () {
+        this.$get = function () {
+            var appName = "Angular Js Sample World";
+            return {
+                appname: appName
+            };
+        }
+
+    });
+
 });
